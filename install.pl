c874e60f5fbe870a434716ac8bbcc48f1e3a1ae1
Elliot Wolk <elliot.wolk@gmail.com>
2013-02-23 14:57:02 -0500

    added a simple install script to install acpi_call from github and copy the script to /usr/local/bin

---
+++ install.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $prefix = '/usr/local';
+my $acpiCallGitRepo = 'git://github.com/mkottman/acpi_call.git';
+my $acpiCallTag = "v1.0.0";
+
+sub runOrDie(@){
+  print "@_\n";
+  system @_;
+  die "error running '@_'\n" if $? != 0;
+}
+
+sub main(@){
+  runOrDie "sudo", "cp", "tpacpi-bat", "$prefix/bin";
+
+  my $localRepo = '/tmp/acpi_call';
+  if(not -d $localRepo){
+    runOrDie "git", "clone", $acpiCallGitRepo, $localRepo;
+  }
+  chdir $localRepo;
+  runOrDie "git", "fetch";
+  runOrDie "git", "reset", "HEAD", "--hard";
+  runOrDie "git", "clean", "-fd";
+  runOrDie "git", "checkout", $acpiCallTag;
+  runOrDie "make";
+  runOrDie "sudo", "make", "install";
+}
+
+&main(@ARGV);
